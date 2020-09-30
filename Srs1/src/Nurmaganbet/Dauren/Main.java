40b5b3858d892b897f6c70ac4c680c7ed5b24851
Dauren0 <dauren0601@gmail.com>
2020-09-30 14:43:49 +0600

    Raw version of first

---
+++ Srs1/src/Nurmaganbet/Dauren/Main.java
@@ -0,0 +1,9 @@
+package Nurmaganbet.Dauren;
+import Nurmaganbet.Dauren.Phone;
+public class Main {
+    public static void main(String[] args){
+        Phone Pixel4 = new Phone();
+        System.out.print(Pixel4.model);
+
+    }
+}
