581f9704168a010be97d05fb7e2ba5fab02c26b2
Dauren0 <dauren0601@gmail.com>
2020-10-05 20:52:58 +0600

    Finished ex1 of Srs1

--- Srs1/Ex1/src/Nurmaganbet/Dauren/Phone.java
+++ Srs1/Ex1/src/Nurmaganbet/Dauren/Phone.java
@@ -1,32 +1,46 @@
+/**Задания:
+ * a) Создайте класс Phone, который содержит переменные number, model и weight.
+ * б) Создайте три экземпляра этого класса.
+ * в) Выведите на консоль значения их переменных.
+ * г) Добавить в класс Phone методы: receiveCall, имеет один параметр – имя звонящего. Выводит на консоль сообщение “Звонит {name}”. getNumber – возвращает номер телефона. Вызвать эти методы для каждого из объектов.
+ * д) Добавить конструктор в класс Phone, который принимает на вход три параметра для инициализации переменных класса - number, model и weight.
+ * е) Добавить конструктор, который принимает на вход два параметра для инициализации переменных класса - number, model.
+ * ж) Добавить конструктор без параметров.
+ * з) Вызвать из конструктора с тремя параметрами конструктор с двумя.
+ * и) Добавьте перегруженный метод receiveCall, который принимает два параметра - имя звонящего и номер телефона звонящего. Вызвать этот метод.
+ */
 package Nurmaganbet.Dauren;
 
 public class Phone {
-    String number = "+7xxxxxxxxxx";
-    String model = "xxx";
-    double weight = 123.7;
+    String number;
+    String model;
+    double weight;
 
-    public Phone(String number, String model, double weight) {
+    public Phone() { }//конструктор без параметров.
+
+    public Phone(String number, String model) {//конструктор, который принимает на вход два параметра для инициализации переменных класса
         this.number = number;
         this.model = model;
+    }
+
+
+    public Phone(String number, String model, double weight) {//конструктор с тремя параметрами который вызывает конструктор с двумя
+        this(number,model);
         this.weight = weight;
-    }
-
-    public Phone(String number, String model) {
-        this.number = number;
-        this.model = model;
-    }
-
-    public Phone() {
     }
 
 
     public void receiveCall(String name) {
-        System.out.println("Звонит " + name);
+        System.out.println("/////////////////////////");
+        System.out.println("//// Звонит " + name+"////");
+        System.out.println("////////////\uD83D\uDCDE///////////");
     }
 
     public void receiveCall(String name, String number) {
-        System.out.println("Звонит " + name);
-        System.out.println(number);
+        System.out.println("/////////////////////////");
+        System.out.println("//// Звонит " + name+"////");
+        System.out.println("////  "+number+"  /////");
+        System.out.println("////////////\uD83D\uDCDE///////////");
     }
 
     public String getNumber() {
